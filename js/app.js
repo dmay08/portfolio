e8f86c4b2455db9e907c4b89bac11e4c2bbccd73
danny mayasich <dannymayasich@dannys-MBP.attlocal.net>
2018-12-21 15:28:32 -0800

    name and title animations only

--- js/app.js
+++ js/app.js
@@ -5,3 +5,12 @@ navBarToggle.addEventListener('click', function () {
     
     mainNav.classList.toggle('active');
 });
+
+$(document).ready(function() {
+    $('.progress-bar').waypoint(function() {
+    $('.progress-bar').css({
+    animation: "animate-positive 2s",
+    opacity: "1"
+    });
+    }, { offset: '75%' });
+}
